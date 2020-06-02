668979acd943dd603d6868241bd87dc3f5086e31
undeinf <www.undefined@gmail.com>
2020-06-03 01:08:21 +0530

    Added Todo Code

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,62 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react'
 import ReactDOM from 'react-dom';
 
+//alias for type
+type FormElem = React.FormEvent<HTMLFormElement>
+
+// interface for type reference todos
+interface ITodo{
+    text:string;
+    complete:boolean;
+}
+
 export default function App():JSX.Element {
-    const greet = (name:string):string => `Hello, ${name}`
+    const [value, setValue] = useState<string>('');
+    const [todos, setTodos] = useState<ITodo[]>([]);
+
+    const handleSubmit = (e:FormElem):void => {
+        e.preventDefault();
+        addTodo(value);
+        setValue('');
+    }
+
+    const addTodo = (text:string):void => {
+        const newTodos:ITodo[] = [...todos, {text, complete:false}];
+        setTodos(newTodos);
+    }
+
+    const completeTodo = (index:number):void => {
+        const newTodos:ITodo[] = [...todos];
+        newTodos[index].complete = !newTodos[index].complete;
+        setTodos(newTodos);
+    }
+
+    const removeTodo = (index:number):void => {
+        const newTodos:ITodo[] = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    }
+
     return (
-        <h1>
-            {greet('REACT-TS')}
-        </h1>
+        <Fragment>
+            <h1>Todo List</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={value} onChange={(e)=>setValue(e.target.value)} required />
+                <button type="submit">Add Todo</button>
+            </form>
+            <section>
+                {todos.map((todo:ITodo, index:number) => (
+                    <Fragment key={index}>
+                    <div style={{textDecoration: todo.complete ? 'line-through':''}}>{todo.text}</div>
+                    <button type="button" onClick={() => completeTodo(index)}>
+                        {' '}
+                        {todo.complete ? 'Incomplete': 'Complete'}
+                    </button>
+                    <button type="button" onClick={()=>removeTodo(index)}>&times;</button>
+                    </Fragment>
+                ))}
+            </section>
+        </Fragment>
     )
 }
 
